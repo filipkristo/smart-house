0bc3218a60e1a665bcf833c7753efd6cfad5d1d9
Filip Krišto <filipkristo@windowslive.com>
2017-01-08 22:42:59 +0100

    python & LIRC config

--- Python/remote.py
+++ Python/remote.py
@@ -3,6 +3,7 @@
 import lirc
 import time
 import requests
+import os
 import RPi.GPIO as GPIO
 
 sockid=lirc.init("dev", blocking = False)
@@ -20,35 +21,34 @@ while True:
             requests.get('http://10.110.166.90:8081/api/SmartHouse/VolumeUp')
         elif "KEY_VOLUMEDOWN" in codeIR[0]:
             requests.get('http://10.110.166.90:8081/api/SmartHouse/VolumeDown')
-        elif "KEY_UP" in codeIR[0]:
-            requests.get('http://10.110.166.90:8081/api/Pandora/NextStation')
-        elif "KEY_DOWN" in codeIR[0]:
-            requests.get('http://10.110.166.90:8081/api/Pandora/PrevStation')
-        elif "KEY_RIGHT" in codeIR[0]:
+        elif "KEY_FORWARD" in codeIR[0]:
             requests.get('http://10.110.166.90:8081/api/SmartHouse/Next')
-        elif "KEY_LEFT" in codeIR[0]:
+        elif "KEY_REWIND" in codeIR[0]:
             requests.get('http://10.110.166.90:8081/api/SmartHouse/Prev')
         elif "KEY_POWER" in codeIR[0]:
-            requests.get('http://10.110.166.90:8081/api/SmartHouse/TurnOn')
-        elif "KEY_POWER2" in codeIR[0]:
-            requests.get('http://10.110.166.90:8081/api/SmartHouse/TurnOff')
+            requests.get('http://10.110.166.90:8081/api/SmartHouse/Power')
         elif "KEY_PLAY" in codeIR[0]:
             requests.get('http://10.110.166.90:8081/api/SmartHouse/Play')
-        elif "KEY_1" in codeIR[0]:
+        elif "KEY_UP" in codeIR[0]:
+            requests.get('http://10.110.166.90:8081/api/Remote/Up')
+        elif "KEY_DOWN" in codeIR[0]:
+            requests.get('http://10.110.166.90:8081/api/Remote/Down')
+        elif "KEY_FN_F1" in codeIR[0]:
             requests.get('http://10.110.166.90:8081/api/SmartHouse/Pandora')
-        elif "KEY_2" in codeIR[0]:
+        elif "KEY_FN_F2" in codeIR[0]:
             requests.get('http://10.110.166.90:8081/api/SmartHouse/Music')
-        elif "KEY_3" in codeIR[0]:
+        elif "KEY_FN_F3" in codeIR[0]:
             requests.get('http://10.110.166.90:8081/api/SmartHouse/XBox')
-        elif "KEY_4" in codeIR[0]:
+        elif "KEY_FN_F4" in codeIR[0]:
             requests.get('http://10.110.166.90:8081/api/SmartHouse/TV')
-        elif "KEY_6" in codeIR[0]:
-            requests.get('http://10.110.166.90:8081/api/Pandora/Tired')
-        elif "KEY_7" in codeIR[0]:
-            requests.get('http://10.110.166.90:8081/api/Pandora/ThumbDown')
-        elif "KEY_8" in codeIR[0]:
+        elif "KEY_KP1" in codeIR[0]:
+            requests.get('http://10.110.166.90:8081/api/Remote/Love')
+        elif "KEY_KP2" in codeIR[0]:
+            requests.get('http://10.110.166.90:8081/api/Remote/Ban')
+        elif "KEY_KP3" in codeIR[0]:
             requests.get('http://10.110.166.90:8081/api/SmartHouse/Notify')
-        elif "KEY_9" in codeIR[0]:
-            requests.get('http://10.110.166.90:8081/api/Pandora/ThumbUp')
+        else:
+            requests.get('http://10.110.166.90:8081/api/SmartHouse/TVCommand?c=' + codeIR[0])            
     time.sleep(0.02)
     GPIO.output(21,GPIO.LOW)
+
