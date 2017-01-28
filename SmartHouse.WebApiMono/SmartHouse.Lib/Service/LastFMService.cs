9c6bb028ec0aa4333a964a401041c7de61b8d7c6
Filip Krišto <filipkristo@windowslive.com>
2017-01-28 17:26:03 +0100

    Last.FM save access token

--- SmartHouse.WebApiMono/SmartHouse.Lib/Service/LastFMService.cs
+++ SmartHouse.WebApiMono/SmartHouse.Lib/Service/LastFMService.cs
@@ -4,11 +4,14 @@ using IF.Lastfm.Core.Api;
 using IF.Lastfm.Core.Objects;
 using System.Linq;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
 
 namespace SmartHouse.Lib
 {
-	public class LastFMService : ILastFMService
-	{
+    public class LastFMService : ILastFMService
+    {
         private const string API_KEY = "XXXX";
         private const string API_SECRET = "XXXX";
 
@@ -16,52 +19,82 @@ namespace SmartHouse.Lib
         private const string PASSWORD = "XXXX";
 
         public LastFMService()
-		{
-			
-		}
+        {
 
-		public async Task<string> StartScrobble(SongDetails song)
-		{
-			using (var client = new LastfmClient(API_KEY, API_SECRET))
-			{
-				await client.Auth.GetSessionTokenAsync(USERNAME, PASSWORD);
+        }
 
-				var scrobble = new Scrobble(song.Artist, song.Album, song.Song, DateTime.UtcNow);
-				var response = await client.Scrobbler.ScrobbleAsync(scrobble);
+        private async Task Authenticate(LastfmClient client)
+        {
+            var fileName = "last.fm";
 
-				return response.Status.ToString();
-			}
-		}
+            if (!File.Exists(fileName))
+            {
+                var result = await client.Auth.GetSessionTokenAsync(USERNAME, PASSWORD);
 
-		public async Task<string> UpdateNowPlaying(SongDetails song)
-		{
-			using (var client = new LastfmClient(API_KEY, API_SECRET))
-			{
-				await client.Auth.GetSessionTokenAsync(USERNAME, PASSWORD);
+                if (client.Auth.Authenticated)
+                {
+                    var json = JsonConvert.SerializeObject(client.Auth.UserSession);
+                    File.WriteAllText("last.fm", json, Encoding.UTF8);
+                }
+            }
+            else
+            {
+                var json = File.ReadAllText(fileName, Encoding.UTF8);
+                var session = JsonConvert.DeserializeObject(json, typeof(LastUserSession)) as LastUserSession;
 
-				var scrobble = new Scrobble(song.Artist, song.Album, song.Song, DateTime.UtcNow);
-				var response = await client.Track.UpdateNowPlayingAsync(scrobble);
+                client.Auth.LoadSession(session);
 
-				return response.Status.ToString();
-			}
-		}
+                if (!client.Auth.Authenticated)
+                {
+                    File.Delete(fileName);
+                    throw new Exception("Last.FM Authenticate problem");
+                }
 
-		public async Task<string> LoveSong(string artistName, string trackName)
-		{
-			using (var client = new LastfmClient(API_KEY, API_SECRET))
-			{
-				await client.Auth.GetSessionTokenAsync(USERNAME, PASSWORD);
+            }
+        }
 
-				var response = await client.Track.LoveAsync(trackName, artistName);
-				return response.Status.ToString();
-			}
-		}
+        public async Task<string> StartScrobble(SongDetails song)
+        {
+            using (var client = new LastfmClient(API_KEY, API_SECRET))
+            {
+                await Authenticate(client);
+
+                var scrobble = new Scrobble(song.Artist, song.Album, song.Song, DateTime.UtcNow);
+                var response = await client.Scrobbler.ScrobbleAsync(scrobble);
+
+                return response.Status.ToString();
+            }
+        }
+
+        public async Task<string> UpdateNowPlaying(SongDetails song)
+        {
+            using (var client = new LastfmClient(API_KEY, API_SECRET))
+            {
+                await Authenticate(client);
+
+                var scrobble = new Scrobble(song.Artist, song.Album, song.Song, DateTime.UtcNow);
+                var response = await client.Track.UpdateNowPlayingAsync(scrobble);
+
+                return response.Status.ToString();
+            }
+        }
+
+        public async Task<string> LoveSong(string artistName, string trackName)
+        {
+            using (var client = new LastfmClient(API_KEY, API_SECRET))
+            {
+                await Authenticate(client);
+
+                var response = await client.Track.LoveAsync(trackName, artistName);
+                return response.Status.ToString();
+            }
+        }
 
         public async Task<string> UnloveSong(string artistName, string trackName)
         {
             using (var client = new LastfmClient(API_KEY, API_SECRET))
             {
-                await client.Auth.GetSessionTokenAsync(USERNAME, PASSWORD);
+                await Authenticate(client);
 
                 var response = await client.Track.UnloveAsync(trackName, artistName);
                 return response.Status.ToString();
@@ -69,33 +102,33 @@ namespace SmartHouse.Lib
         }
 
         public async Task<LastTrack> GetSongInfo(string artistName, string trackName)
-		{
-			using (var client = new LastfmClient(API_KEY, API_SECRET))
-			{
-				await client.Auth.GetSessionTokenAsync(USERNAME, PASSWORD);
+        {
+            using (var client = new LastfmClient(API_KEY, API_SECRET))
+            {
+                await Authenticate(client);
 
-				var response = await client.Track.GetInfoAsync(trackName, artistName, USERNAME);
+                var response = await client.Track.GetInfoAsync(trackName, artistName, USERNAME);
 
-				return response.Content;
-			}
-		}
+                return response.Content;
+            }
+        }
 
-		public async Task<List<LastTrack>> GetTopTracks()
-		{
-			using (var client = new LastfmClient(API_KEY, API_SECRET))
-			{
-				await client.Auth.GetSessionTokenAsync(USERNAME, PASSWORD);
+        public async Task<List<LastTrack>> GetTopTracks()
+        {
+            using (var client = new LastfmClient(API_KEY, API_SECRET))
+            {
+                await Authenticate(client);
 
-				var response = await client.Chart.GetTopTracksAsync(1, 50);
-				return response.Content.ToList();
-			}
-		}
+                var response = await client.Chart.GetTopTracksAsync(1, 50);
+                return response.Content.ToList();
+            }
+        }
 
         public async Task<LastAlbum> GetAlbumInfo(string artist, string album)
         {
             using (var client = new LastfmClient(API_KEY, API_SECRET))
             {
-                await client.Auth.GetSessionTokenAsync(USERNAME, PASSWORD);
+                await Authenticate(client);
 
                 var response = await client.Album.GetInfoAsync(artist, album, true);
                 return response.Content;
@@ -106,7 +139,7 @@ namespace SmartHouse.Lib
         {
             using (var client = new LastfmClient(API_KEY, API_SECRET))
             {
-                await client.Auth.GetSessionTokenAsync(USERNAME, PASSWORD);
+                await Authenticate(client);
 
                 var response = await client.Artist.GetInfoAsync(artist: artist, autocorrect: true);
                 return response.Content;
@@ -117,7 +150,7 @@ namespace SmartHouse.Lib
         {
             using (var client = new LastfmClient(API_KEY, API_SECRET))
             {
-                await client.Auth.GetSessionTokenAsync(USERNAME, PASSWORD);
+                await Authenticate(client);
 
                 var response = await client.Artist.GetSimilarAsync(artist, true, limit);
                 return response.Content;
