faa881f0a21e0a66cd44f27c98ba1726e29ddcc1
Filip Krišto <filipkristo@windowslive.com>
2017-02-19 19:04:30 +0100

    fix build error

---
+++ SmartHouse.WebApiMono/SmartHouse.Lib/Model/ProcessResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.Lib
+{
+    public class ProcessResult
+    {
+        public string Message { get; set; }
+        public string Error { get; set; }
+
+        public override string ToString()
+        {
+            return $"Error: {Error}{Environment.NewLine}Output: {Message}";
+        }
+    }
+}
