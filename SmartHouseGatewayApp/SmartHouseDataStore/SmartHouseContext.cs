dc3eadafe1ed24408721ed3e48d6999abea3cd6b
dbaric99 <damjana.baric1999@gmail.com>
2019-08-05 19:59:56 +0200

    Closed #1

---
+++ SmartHouseGatewayApp/SmartHouseDataStore/SmartHouseContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SmartHouseDataStore.Entities;
+
+namespace SmartHouseDataStore
+{
+    public class SmartHouseContext:DbContext
+    {
+        public DbSet<Command> Commands { get; set; }
+        public DbSet<Condition> Conditions { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<DeviceSetting> DeviceSettings { get; set; }
+        public DbSet<DeviceState> DeviceStates { get; set; }
+        public DbSet<DeviceType> DeviceTypes { get; set; }
+        public DbSet<Routine> Routines { get; set; }
+        public DbSet<RoutineCommand> RoutineCommands { get; set; }
+        public DbSet<SmartHouseState> SmartHouseStates { get;set; }
+    }
+}
