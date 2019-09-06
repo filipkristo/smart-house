1ac79cf540644fb109455b04f9199410e339e38c
Filip Krišto <filipkristo@windowslive.com>
2019-09-06 09:08:31 +0200

    add context seed tests

---
+++ SmartHouseGatewayApp/SmartHouseDataStoreTests/InMemoryContextFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SmartHouseDataStore;
+
+namespace SmartHouseDataStoreTests
+{
+    public class InMemoryContextFixture : IDisposable
+    {
+        public SmartHouseContext Context { get; }
+
+        public InMemoryContextFixture()
+        {
+            var options = new DbContextOptionsBuilder<SmartHouseContext>()
+                .UseInMemoryDatabase(databaseName: "Add_writes_to_database")
+                .Options;
+
+            Context = new SmartHouseContext(options);
+            Context.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
